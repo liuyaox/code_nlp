884072295df9d4a4d47c0d996ff6f549741704d3
liuyaa58 <siberiawolf61@hotmail.com>
2019-03-26 21:59:14 +0800

    add model_utils.py

--- Model/model_utils.py
+++ Model/model_utils.py
@@ -5,29 +5,35 @@ Created on Tue Mar 25 14:02:18 2019
 @author: liuyao8
 """
 
-
-def model_eval(model, test_x, test_y, y_thresh=0.5):
-	""" 模型评估   Metrics: Accuracy, Precision, Recall, F1 Score"""
-	ypred = [int(x[0] > y_thresh) for x in model.predict(test_x)]
+def ypred_eval(ytrues, ypreds, detail=False):
 	TP, FP, TN, FN = 0, 0, 0, 0
-	for ytrue, yhat in zip(test_y, ypred):
-		if ytrue == 1 and yhat == 1:
+	for ytrue, ypred in zip(ytrues, ypreds):
+		if ytrue == 1 and ypred == 1:
 			TP += 1
-		elif ytrue == 0 and yhat == 1:
+		elif ytrue == 0 and ypred == 1:
 			FP += 1
-		elif ytrue == 0 and yhat == 0:
+		elif ytrue == 0 and ypred == 0:
 			TN += 1
-		elif ytrue == 1 and yhat == 0:
+		elif ytrue == 1 and ypred == 0:
 			FN += 1
-	accuracy = (TP + TN) / res.shape[0]
+	accuracy = (TP + TN) / len(ypreds)
 	precision = TP / (TP + FP)
 	recall = TP / (TP + FN)
 	f1 = precision * recall / (precision + recall)
+	if detail:
+		return accuracy, precision, recall, f1, TP, FP, TN, FN
+	else:
+		return accuracy, precision, recall, f1
+
+
+def model_eval(model, test_x, test_y, y_thresh=0.5):
+	""" 模型评估   Metrics: Accuracy, Precision, Recall, F1 Score"""
+	ypreds = [int(x[0] > y_thresh) for x in model.predict(test_x)]
+	accuracy, precision, recall, f1, TP, FP, TN, FN = ypred_eval(test_y, ypreds, detail=True)
 	print(f'TP: {TP}  FP: {FP}  TN: {TN}  FN: {FN}')
 	print('Accuracy:\t' + str(accuracy))
 	print('Precision:\t' + str(precision))
 	print('Recall:\t' + str(recall))
 	print('f1 score:\t' + str(f1))
 	return accuracy, precision, recall, f1
-	
 	
