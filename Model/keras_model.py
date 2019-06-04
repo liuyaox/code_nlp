706a301a6387209c3a3191afa18b4defa5ec1796
liuyaox <siberiawolf61@hotmail.com>
2019-06-04 16:09:18 +0800

    add get_session

--- Model/keras_model.py
+++ Model/keras_model.py
@@ -12,6 +12,8 @@ from keras.layers import Embedding
 from keras.initializers import Constant
 from keras.callbacks import ModelCheckpoint
 from keras.utils import multi_gpu_model
+from keras import backend as K
+import tensorflow as tf
 import os
 
 
@@ -20,7 +22,18 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
 
 
+def get_session(gpu_fraction=1.0):
+    """GPU使用设置，何时需要？"""
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    if num_threads:
+        config=tf.ConfigProto(gpu_options=gpu_options, intra_op_parallelism_threads=num_threads)
+    else:
+        config=tf.ConfigProto(gpu_options=gpu_options)
+    return tf.Session(config=config)
 
+
+    
 def get_embedding_layer(initializer='constant', word2index=None, word2vector=None, word_embedding_spare=None, 
                   embedding_dim=64, vocab_len=None, trainable=False, name=None):
     """
@@ -59,7 +72,7 @@ def get_embedding_layer(initializer='constant', word2index=None, word2vector=Non
 
 
 class ParallelModelCheckpoint(ModelCheckpoint):
-	"""自定义Checkpoint子类，保存模型model的参数(合并多GPU训练得到的参数)"""
+    """自定义Checkpoint子类，保存模型model的参数(合并多GPU训练得到的参数)"""
     def __init__(self, model, filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1):
         self.single_model = model
         super(ParallelModelCheckpoint, self).__init__(filepath, monitor, verbose, save_best_only, save_weights_only, mode, period)
@@ -70,22 +83,25 @@ class ParallelModelCheckpoint(ModelCheckpoint):
 
 
 if __name__ == '__main__':
+    
+    # 加载模型前设置session，有时候不需要？
+    K.set_session(get_session())
 
-	model = xxx
-	checkpoint = ParallelModelCheckpoint(
-		model, 
-		filepath= 'dapei_{epoch:02d}_{val_loss:.4f}.h5', 
-		monitor='val_loss', 
-		save_best_only=False, 
-		save_weights_only=False)
-	
-	model2 = multi_gpu_model(model, 2)
-	model2.compile(xxx)
-	history = model2.fit(xxx, callbacks=[checkpoint])
-	model.save_weights('xxx.h5')				# 注意是model，而非model2
-	
-	print('------------------------ Result -----------------------')
-	accs, val_accs = history.history['acc'], history.history['val_acc']
-	imax = np.argmax(val_accs)
-	print('acc: ' + str(round(accs[imax] * 100, 2)) + '\t' + 'val_acc: ' + str(round(val_accs[imax] * 100, 2)))
-	#print('test_acc: ' + str(round(model.evaluate(test_x, test_y)[1] * 100, 2)))
+    model = xxx
+    checkpoint = ParallelModelCheckpoint(
+        model, 
+        filepath= 'dapei_{epoch:02d}_{val_loss:.4f}.h5', 
+        monitor='val_loss', 
+        save_best_only=False, 
+        save_weights_only=False)
+    
+    model2 = multi_gpu_model(model, 2)
+    model2.compile(xxx)
+    history = model2.fit(xxx, callbacks=[checkpoint])
+    model.save_weights('xxx.h5')				# 注意是model，而非model2
+
+    print('------------------------ Result -----------------------')
+    accs, val_accs = history.history['acc'], history.history['val_acc']
+    imax = np.argmax(val_accs)
+    print('acc: ' + str(round(accs[imax] * 100, 2)) + '\t' + 'val_acc: ' + str(round(val_accs[imax] * 100, 2)))
+    #print('test_acc: ' + str(round(model.evaluate(test_x, test_y)[1] * 100, 2)))
